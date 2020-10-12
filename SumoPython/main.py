1aacda922b4574a0c26469a2f00e3ce38e82fb09
AAYUSH NARWAY <44194937+NARWAYAAYUSH@users.noreply.github.com>
2020-10-12 16:43:47 +0530

    Add files via upload
    nothing upgraded, little changes

--- SumoPython/main.py
+++ SumoPython/main.py
@@ -19,16 +19,33 @@ traci.start(sumoCmd)
 
 print("started")
 
+# change here
+startEdge = '-24241790#2'
+endEdge = '-4588217#1'
+routeID = "ambulanceRoute"
+ambulanceID = "ambulance"
+viewID = "View #0"
+
 step = 0
 delayTime = 0
+isAmbulancePresent = False
 
 while traci.simulation.getMinExpectedNumber() > 0:
 
     if step == 100:
-        tm.addAmbulance()
-        delayTime = 0.15
+    #     trafficLightID = "cluster_1828018077_1828018093_1828018094_1828018095_1840326399_1840326421_262469034_29123671_29123672_3832550626_3832550632_6795935360_6795935361_6795935362_967017837_967017877"
+    #
+    #     controlledLanes = traci.trafficlight.getControlledLanes(trafficLightID)
+    #     controlledLinks = traci.trafficlight.getControlledLinks(trafficLightID)
+    #     state = traci.trafficlight.getRedYellowGreenState(trafficLightID)
+    #     currentPhase = traci.trafficlight.getPhase(trafficLightID)
+    #     program = traci.trafficlight.getCompleteRedYellowGreenDefinition(trafficLightID)
 
-    time.sleep(delayTime)
+        tm.addAmbulance(ambulanceID, routeID, viewID, startEdge, endEdge)
+        delayTime = 0.15
+        isAmbulancePresent = True
+        time.sleep(delayTime)
+
     traci.simulationStep()
     step += 1
 
--- SumoPython/traciMethods.py
+++ SumoPython/traciMethods.py
@@ -1,20 +1,13 @@
 import traci
 
 
-def addAmbulance():
+def addAmbulance(ambulanceID, routeID, viewID, startEdge, endEdge):
 
-    routeID = "ambulanceRoute"
+    traci.route.add(routeID, [startEdge, endEdge])
 
-    # change here  -give edges accordingly
-    traci.route.add(routeID, ['-24241790#2', '-4588217#5'])
-
-    ambulanceID = "ambulance"
-
-    traci.vehicle.add(ambulanceID, "ambulanceRoute", typeID="truck_emergency")
+    traci.vehicle.add(ambulanceID, routeID, typeID="truck_emergency")
     traci.vehicle.setVehicleClass(ambulanceID, "emergency")
     traci.vehicle.setShapeClass(ambulanceID, "emergency")
-
-    viewID = "View #0"
 
     traci.gui.trackVehicle(viewID, ambulanceID)
     traci.gui.setZoom(viewID, 3500)
